a5d0cb5987846ac3c777d04af0f723eb14ca8688
Michael Quaas <michael.quaas@betterplace.org>
2015-07-15 18:17:00 +0200

    Implements new response checksum

--- lib/wirecard_checkout_page/response_checksum.rb
+++ lib/wirecard_checkout_page/response_checksum.rb
@@ -1,64 +1,25 @@
 # encoding: utf-8
-
-require 'wirecard_checkout_page/value_handling'
+require 'wirecard_checkout_page/utils'
 
 module WirecardCheckoutPage
   class ResponseChecksum
-    include WirecardCheckoutPage::Utils
-    include WirecardCheckoutPage::ValueHandling
+    attr_reader :params
 
-    def initialize(values = {})
-      @values = stringify_keys(values)
-      @secret = @values.delete('secret') or
-        raise WirecardCheckoutPage::ValueMissing, 'value "secret" is missing'
-      # This rails form value is escaped as an html entity by
-      # WirecardCheckoutPage, so set it back to the original UTF-8 here if it
-      # exists:
-      @values['utf8'] and @values['utf8'] = '✓'
-      @values.freeze
-      reset_missing_keys
-    end
-
-    attr_reader :values
-
-    attr_reader :expected_fingerprint
-
-    attr_reader :computed_fingerprint
-
-    def fingerprint
-      values = @values.dup
-      values['secret'] ||= @secret
-      if seed = responseFingerprintSeed(responseFingerprintOrder(values), values)
-        Digest::MD5.hexdigest seed
-      end
+    def initialize(params)
+      @params = WirecardCheckoutPage::Utils.stringify_keys(params)
     end
 
     def valid?
-      reset_missing_keys
-      @expected_fingerprint = values.fetch('responseFingerprint') do |k|
-        add_missing_key k
-      end
-      @computed_fingerprint = fingerprint
-      !missing_keys? && computed_fingerprint == @expected_fingerprint
+      params['responseFingerprint'] == computed_fingerprint
     end
 
-    private
-
-    def responseFingerprintOrder(values)
-      order = values.fetch('responseFingerprintOrder') do |k|
-        add_missing_key k
-        return []
-      end
-      order.split(',')
+    def fingerprint_string
+      params['responseFingerprintOrder'].split(',').map {|key| params[key.to_s] }.join
     end
 
-    def responseFingerprintSeed(keys, values)
-      fingerprint = keys.map do |k|
-        values.fetch(k) do
-          add_missing_key k
-        end
-      end * ''
-      fingerprint unless missing_keys?
+    def computed_fingerprint
+      Digest::MD5.hexdigest fingerprint_string
     end
+
   end
 end
--- spec/wirecard_checkout_page/response_checksum_spec.rb
+++ spec/wirecard_checkout_page/response_checksum_spec.rb
@@ -2,127 +2,61 @@ require 'spec_helper'
 require 'wirecard_checkout_page'
 
 describe WirecardCheckoutPage::ResponseChecksum do
-  let :secret do
-    'SOMESECRET'
-  end
+  describe '#valid?' do
+    context 'with valid parameters' do
+      it 'is valid' do
+        fingerprint_order           = 'amount,currency,paymentType,financialInstitution,language,orderNumber,paymentState,authenticated,anonymousPan,expiry,maskedPan,gatewayReferenceNumber,gatewayContractNumber,secret,responseFingerprintOrder'
+        expected_fingerprint_string = '50.00''EUR''CCARD''Visa''de''8300664''SUCCESS''No''1122''06/2018''405911******1122''C101361143697423285286''000000316159CED9''SECRET'"#{fingerprint_order}"
 
-  let :customer_id do
-    'SOMECUSTOMERID'
-  end
+        params = {
+          secret:                    'SECRET',
+          'amount'                   => '50.00',
+          'currency'                 => 'EUR',
+          'paymentType'              => 'CCARD',
+          'financialInstitution'     => 'Visa',
+          'language'                 => 'de',
+          'orderNumber'              => '8300664',
+          'paymentState'             => 'SUCCESS',
+          'authenticated'            => 'No',
+          'anonymousPan'             => '1122',
+          'expiry'                   => '06/2018',
+          'maskedPan'                => '405911******1122',
+          'gatewayReferenceNumber'   => 'C101361143697423285286',
+          'gatewayContractNumber'    => '000000316159CED9',
+          'responseFingerprintOrder' => fingerprint_order,
+          'responseFingerprint'      => 'd1e7ecba3980ca2da4954b9d154c1e1e',
+        }
+        checksum = described_class.new(params)
+        expect(checksum).to be_valid
+      end
+    end
 
-  let :shop_id do
-    'someshopid'
-  end
+    context 'with invalid parameters' do
+      it 'is not valid' do
+        fingerprint_order           = 'amount,currency,paymentType,financialInstitution,language,orderNumber,paymentState,authenticated,anonymousPan,expiry,maskedPan,gatewayReferenceNumber,gatewayContractNumber,secret,responseFingerprintOrder'
+        expected_fingerprint_string = '50.00''EUR''CCARD''Visa''de''8300664''SUCCESS''No''1122''06/2018''405911******1122''C101361143697423285286''000000316159CED9''SECRET'"#{fingerprint_order}"
 
-  it "recognizes a correct response" do
-    response_params = {
-      secret:                  secret,
-      fingerprint_keys:        WirecardCheckoutPage::RequestChecksum::FINGERPRINT_KEYS + %w[shopId],
-      customerId:              customer_id,
-      shopId:                  shop_id,
-      "amount"                 => "28.95",
-      "currency"               => "EUR",
-      "paymentType"            => "CCARD",
-      "financialInstitution"   => "MC",
-      "language"               => "de",
-      "orderNumber"            => "7739491",
-      "paymentState"           => "SUCCESS",
-      "utf8"                   => "&#10003;",
-      "authenticity_token"     => "bnz1fHxcCYD9jdPiNIEl7yJExRetWWAOQPopmjYksFc=",
-      "commit"                 => "Bezahlen",
-      "authenticated"          => "No",
-      "anonymousPan"           => "0002",
-      "expiry"                 => "01/2013",
-      "cardholder"             => "Foorian Bar",
-      "maskedPan"              => "950000******0002",
-      "gatewayReferenceNumber" => "DGW_7739491_RN",
-      "gatewayContractNumber"  => "DemoContractNumber123",
-      "responseFingerprintOrder"=>"amount,currency,paymentType,"\
-        "financialInstitution,language,orderNumber,paymentState,utf8,"\
-        "authenticity_token,commit,authenticated,anonymousPan,expiry,"\
-        "cardholder,maskedPan,gatewayReferenceNumber,gatewayContractNumber,"\
-        "secret,responseFingerprintOrder",
-      "responseFingerprint" => "8a1319b4a097d5a9157f479b11e8f5ae",
-      "challenge_offer_id"  => "0dd916e49abd1935c2dc084bae2a57b8"
-    }
-    checksum = WirecardCheckoutPage::ResponseChecksum.new(response_params)
-    checksum.valid?
-    expect(checksum.computed_fingerprint).to eq '8a1319b4a097d5a9157f479b11e8f5ae'
-    expect(checksum).to be_valid
-  end
-
-  it "fails check on an incorrect response" do
-    response_params = {
-      secret:                  secret,
-      fingerprint_keys:        WirecardCheckoutPage::RequestChecksum::FINGERPRINT_KEYS + %w[shopId],
-      customerId:              customer_id,
-      shopId:                  shop_id,
-      "amount"                 => "28.95",
-      "currency"               => "EUR",
-      "paymentType"            => "CCARD",
-      "financialInstitution"   => "MC",
-      "language"               => "de",
-      "orderNumber"            => "7739491",
-      "paymentState"           => "SUCCESS",
-      "utf8"                   => "&#10003;",
-      "authenticity_token"     => "bnz1fHxcCYD9jdPiNIEl7yJExRetWWAOQPopmjYksFc=",
-      "commit"                 => "Bezahlen",
-      "authenticated"          => "No",
-      "anonymousPan"           => "0002",
-      "expiry"                 => "01/2013",
-      "cardholder"             => "Foorian Bar",
-      "maskedPan"              => "950000******0002",
-      "gatewayReferenceNumber" => "DGW_7739491_RN",
-      "gatewayContractNumber"  => "DemoContractNumber123",
-      "responseFingerprintOrder"=>"amount,currency,paymentType,"\
-        "financialInstitution,language,orderNumber,paymentState,utf8,"\
-        "authenticity_token,commit,authenticated,anonymousPan,expiry,"\
-        "cardholder,maskedPan,gatewayReferenceNumber,gatewayContractNumber,"\
-        "secret,responseFingerprintOrder",
-      "responseFingerprint" => "666c9c80495703dabfc08434d2e99af0",
-      "challenge_offer_id"  => "0dd916e49abd1935c2dc084bae2a57b8"
-    }
-    expect(WirecardCheckoutPage::ResponseChecksum.new(response_params)).
-      to_not be_valid
-  end
-
-  it "fails check on a response with missing keys" do
-    response_params = {
-      secret:                  secret,
-      fingerprint_keys:        WirecardCheckoutPage::RequestChecksum::FINGERPRINT_KEYS + %w[shopId],
-      customerId:              customer_id,
-      shopId:                  shop_id,
-      "currency"               => "EUR",
-      "paymentType"            => "CCARD",
-      "financialInstitution"   => "MC",
-      "language"               => "de",
-      "orderNumber"            => "7739491",
-      "paymentState"           => "SUCCESS",
-      "utf8"                   => "&#10003;",
-      "authenticity_token"     => "bnz1fHxcCYD9jdPiNIEl7yJExRetWWAOQPopmjYksFc=",
-      "commit"                 => "Bezahlen",
-      "authenticated"          => "No",
-      "anonymousPan"           => "0002",
-      "expiry"                 => "01/2013",
-      "cardholder"             => "Foorian Bar",
-      "maskedPan"              => "950000******0002",
-      "gatewayReferenceNumber" => "DGW_7739491_RN",
-      "gatewayContractNumber"  => "DemoContractNumber123",
-      "responseFingerprintOrder"=>"amount,currency,paymentType,"\
-        "financialInstitution,language,orderNumber,paymentState,utf8,"\
-        "authenticity_token,commit,authenticated,anonymousPan,expiry,"\
-        "cardholder,maskedPan,gatewayReferenceNumber,gatewayContractNumber,"\
-        "secret,responseFingerprintOrder",
-      "responseFingerprint" => "666c9c80495703dabfc08434d2e99af0",
-      "challenge_offer_id"  => "0dd916e49abd1935c2dc084bae2a57b8"
-    }
-    checksum = WirecardCheckoutPage::ResponseChecksum.new(response_params)
-    expect(checksum).to_not be_valid
-    expect(checksum).to be_missing_keys
-    expect(checksum.missing_keys?).to eq %w[amount]
-  end
-
-  it "fails check in an empty response" do
-    expect(WirecardCheckoutPage::ResponseChecksum.new(secret: secret)).to_not be_valid
+        params = {
+          secret:                    'SECRET',
+          'amount'                   => '21121221250.00',
+          'currency'                 => 'EUR',
+          'paymentType'              => 'CCARD',
+          'financialInstitution'     => 'Visa',
+          'language'                 => 'de',
+          'orderNumber'              => '8300664',
+          'paymentState'             => 'SUCCESS',
+          'authenticated'            => 'No',
+          'anonymousPan'             => '1122',
+          'expiry'                   => '06/2018',
+          'maskedPan'                => '405911******1122',
+          'gatewayReferenceNumber'   => 'C101361143697423285286',
+          'gatewayContractNumber'    => '000000316159CED9',
+          'responseFingerprintOrder' => fingerprint_order,
+          'responseFingerprint'      => 'd1e7ecba3980ca2da4954b9d154c1e1e',
+        }
+        checksum = described_class.new(params)
+        expect(checksum).to_not be_valid
+      end
+    end
   end
 end
